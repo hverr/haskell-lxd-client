aecbefcc6168cc377c1a9475d372cd109a8cd489
Henri Verroken <henriverroken@gmail.com>
2017-05-09 22:53:29 +0000

    Clean up trusted and untrusted access in tests

--- test/Integration/API.hs
+++ test/Integration/API.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RecordWildCards #-}
 module Integration.API where
 
@@ -6,27 +7,38 @@ import Testing
 
 import Data.Default (def)
 
-import Servant.Client (runClientM)
+import Servant.Client (ClientEnv, ClientM, runClientM)
 
 import Network.LXD.Client
 
 apiTester :: Tester IO ()
 apiTester = do
-    testShow "connectToRemote guest"   $ connectToRemote NoClientAuth
-    testShow "connectToRemote trusted" $ connectToRemote DefaultClientAuth
+    testShow "connectToRemote trusted"   $ connectToRemote True
+    testShow "connectToRemote untrusted" $ connectToRemote False
 
-connectToRemote :: MonadIO m => ClientAuth -> Test m ApiConfig
-connectToRemote auth = do
-    client <- remoteHostClient host
-    resp <- assertEitherShow =<< (lift . liftIO $ runClientM apiConfig client)
+connectToRemote :: MonadIO m => Bool -> Test m ApiConfig
+connectToRemote trusted = do
+    resp <- runClientM' apiConfig =<< client
     dat <- assertResponseOK resp
-    case auth of NoClientAuth      -> assertEq Untrusted (authStatus dat)
-                 DefaultClientAuth -> assertEq Trusted   (authStatus dat)
-                 ClientAuthKey _   -> assertEq Untrusted (authStatus dat)
+    if trusted then assertEq Trusted   (authStatus dat)
+               else assertEq Untrusted (authStatus dat)
     return dat
   where
-    host = def { remoteHostHost = "127.0.0.1"
-               , remoteHostClientKey = auth }
+    client | True  <- trusted = trustedClient
+           | False <- trusted = untrustedClient
+
+trustedClient :: (MonadError String m, MonadIO m) => m ClientEnv
+trustedClient = remoteHostClient host
+  where host = def { remoteHostHost = "127.0.0.1"
+                   , remoteHostClientKey = DefaultClientAuth }
+
+untrustedClient :: (MonadError String m, MonadIO m) => m ClientEnv
+untrustedClient = remoteHostClient host
+  where host = def { remoteHostHost = "127.0.0.1"
+                   , remoteHostClientKey = NoClientAuth }
+
+runClientM' :: MonadIO m => ClientM a -> ClientEnv -> Test m a
+runClientM' action client = liftIO (runClientM action client) >>= assertEitherShow
 
 assertResponseOK :: Monad m => Response a -> Test m a
 assertResponseOK Response{..}
